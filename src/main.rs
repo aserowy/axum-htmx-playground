c5f20c489fb9f9da1831e931f5fc769218da0f68
aserowy <serowy@hotmail.com>
2023-11-08 08:52:02 +0100

    added basic notification stream support
    Signed-off-by: aserowy <serowy@hotmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,48 @@ use askama::Template;
 use axum::{
     extract::Path,
     http::StatusCode,
-    response::IntoResponse,
+    response::{
+        sse::{Event, KeepAlive},
+        IntoResponse, Sse,
+    },
     routing::{delete, get},
-    Form, Router,
+    Extension, Form, Router,
 };
 use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
-use tokio::time::{sleep, Duration};
+use serde_json::json;
+use std::{convert::Infallible, net::SocketAddr};
+use tokio::{
+    sync::broadcast::{channel, Sender},
+    time::{sleep, Duration},
+};
+use tokio_stream::{wrappers::BroadcastStream, Stream, StreamExt as _};
 use uuid::Uuid;
+
+pub type NotificationSender = Sender<Notification>;
+
+#[derive(Clone, Debug, Serialize)]
+pub enum Severity {
+    Success,
+    Error,
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct Notification {
+    pub id: String,
+    pub severity: Severity,
+    pub message: String,
+}
 
 #[tokio::main]
 async fn main() {
+    let (notification_sender, _) = channel::<Notification>(10);
+
     let router = Router::new()
         .route("/", get(home))
         .route("/entries", get(get_entries).post(post_entry))
-        .route("/entries/:id", delete(delete_entry));
+        .route("/entries/:id", delete(delete_entry))
+        .route("/notifications", get(get_notification_sse))
+        .layer(Extension(notification_sender));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
 
@@ -62,7 +89,7 @@ async fn get_entries() -> impl IntoResponse {
     }
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize, Serialize)]
 struct EntryForm {
     content: String,
 }
@@ -73,8 +100,17 @@ struct EntryTemplate {
     entry: Entry,
 }
 
-async fn post_entry(Form(entry): Form<EntryForm>) -> impl IntoResponse {
-    println!("creating entry with content: {}", entry.content);
+async fn post_entry(
+    Extension(sender): Extension<NotificationSender>,
+    Form(entry): Form<EntryForm>,
+) -> impl IntoResponse {
+    if let Err(_) = sender.send(Notification {
+        id: Uuid::new_v4().to_string(),
+        severity: Severity::Success,
+        message: format!("created entry with content: {}", entry.content),
+    }) {
+        eprintln!("failed to send notification");
+    }
 
     EntryTemplate {
         entry: Entry {
@@ -84,10 +120,45 @@ async fn post_entry(Form(entry): Form<EntryForm>) -> impl IntoResponse {
     }
 }
 
-async fn delete_entry(Path(id): Path<String>) -> impl IntoResponse {
+async fn delete_entry(
+    Extension(sender): Extension<NotificationSender>,
+    Path(id): Path<String>,
+) -> impl IntoResponse {
     let uuid = Uuid::parse_str(&id).unwrap();
 
-    println!("deleting entry with id {}", uuid);
+    if let Err(_) = sender.send(Notification {
+        id: Uuid::new_v4().to_string(),
+        severity: Severity::Error,
+        message: format!("deleted entry with id: {}", uuid),
+    }) {
+        eprintln!("failed to send notification");
+    }
 
     StatusCode::OK
+}
+
+async fn get_notification_sse(
+    Extension(sender): Extension<NotificationSender>,
+) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
+    let subscriber = sender.subscribe();
+    let stream = BroadcastStream::new(subscriber);
+
+    Sse::new(
+        stream
+            .map(|ntfctn| {
+                if let Ok(notification) = ntfctn {
+                    let message = format!("<div>{}</div>", json!(notification));
+                    Event::default().data(message)
+                } else {
+                    let message = "<div>error handling notification</div>".to_string();
+                    Event::default().data(message)
+                }
+            })
+            .map(Ok),
+    )
+    .keep_alive(
+        KeepAlive::new()
+            .interval(Duration::from_secs(5))
+            .text("keep-alive-message"),
+    )
 }
